5fc6a86704fa3f39f867d7caa0646af128ea5a0f
Paddy Zhou <26714434+chonpszhou@users.noreply.github.com>
2026-08-10 17:49:32 +0800

    引导页升级：6页滑动完整介绍（圈子/四类活动/智能筹备/相册海报），带分页圆点与上下步导航

--- miniprogram/pages/onboard/onboard.js
+++ miniprogram/pages/onboard/onboard.js
@@ -1,9 +1,33 @@
 const store = require('../../utils/store')
 
 Page({
+  data: {
+    current: 0,
+    total: 6,
+    pages: [0, 1, 2, 3, 4, 5]
+  },
+
   onLoad() {
     if (store.isOnboardDone()) {
       this.enterApp(false)
+    }
+  },
+
+  onSwiper(e) {
+    this.setData({ current: e.detail.current })
+  },
+
+  next() {
+    if (this.data.current < this.data.total - 1) {
+      this.setData({ current: this.data.current + 1 })
+    } else {
+      this.start()
+    }
+  },
+
+  prev() {
+    if (this.data.current > 0) {
+      this.setData({ current: this.data.current - 1 })
     }
   },
 
